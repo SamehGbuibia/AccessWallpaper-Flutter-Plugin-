1b9fdd42f312b035552267a5fb85549f807f678f
Sameh_Gbuibia <gbuibiasameh@issatso.u-sousse.tn>
2023-04-11 12:54:54 +0100

    release version

--- android/src/main/kotlin/sameh/gbibia/access_wallpaper/AccessWallpaperPlugin.kt
+++ android/src/main/kotlin/sameh/gbibia/access_wallpaper/AccessWallpaperPlugin.kt
@@ -1,12 +1,24 @@
 package sameh.gbibia.access_wallpaper
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import android.annotation.SuppressLint
+import android.app.WallpaperManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.os.ParcelFileDescriptor
+import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.io.ByteArrayOutputStream
+import android.content.Context
 
 /** AccessWallpaperPlugin */
 class AccessWallpaperPlugin: FlutterPlugin, MethodCallHandler {
@@ -15,15 +27,22 @@ class AccessWallpaperPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
-
+  private lateinit var context : Context
+  
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "access_wallpaper")
     channel.setMethodCallHandler(this)
+    context = flutterPluginBinding.getApplicationContext();
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "getWallpaper") {
+      val wallpaper = getWallpaper(call.arguments as Int)
+      if (wallpaper != null) {
+          result.success(wallpaper)
+      } else {
+          result.error("UNAVAILABLE", "Wallpaper not available.", null)
+      }
     } else {
       result.notImplemented()
     }
@@ -31,5 +50,22 @@ class AccessWallpaperPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+  
+  @SuppressLint("MissingPermission")
+  private fun getWallpaper (wallpaperFlag:Int): ByteArray? {
+      val wallpaperManager: WallpaperManager = WallpaperManager.getInstance(context)
+      if (VERSION.SDK_INT >= VERSION_CODES.N) {
+          val wallpaper = wallpaperManager.getWallpaperFile(wallpaperFlag)
+          if(wallpaper!=null){
+              return ParcelFileDescriptor.AutoCloseInputStream(wallpaper).readBytes()
+          }
+      }
+      val d = wallpaperManager.drawable
+      val bitmap = (d as BitmapDrawable).bitmap
+      val stream = ByteArrayOutputStream()
+      bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+      val bitmapdata: ByteArray = stream.toByteArray()
+      return bitmapdata
   }
 }
