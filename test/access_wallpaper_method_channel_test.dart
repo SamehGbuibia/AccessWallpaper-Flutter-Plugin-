4ba04db2e65e6753d29789766009d26f2fdcbdf6
Sameh_Gbuibia <gbuibiasameh@issatso.u-sousse.tn>
2023-04-11 11:48:20 +0100

    remove default test issue

--- test/access_wallpaper_method_channel_test.dart
+++ test/access_wallpaper_method_channel_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:access_wallpaper/access_wallpaper_method_channel.dart';
 
 void main() {
-  MethodChannelAccessWallpaper platform = MethodChannelAccessWallpaper();
   const MethodChannel channel = MethodChannel('access_wallpaper');
 
   TestWidgetsFlutterBinding.ensureInitialized();
--- test/access_wallpaper_test.dart
+++ test/access_wallpaper_test.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:access_wallpaper/access_wallpaper.dart';
 import 'package:access_wallpaper/access_wallpaper_platform_interface.dart';
 import 'package:access_wallpaper/access_wallpaper_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
@@ -9,9 +8,6 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockAccessWallpaperPlatform
     with MockPlatformInterfaceMixin
     implements AccessWallpaperPlatform {
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-
   @override
   Future<Uint8List?> getWallpaper(int wallPaperFlag) {
     throw UnimplementedError();
